71b7bca5893373381475364684c891a650f81f11
Aaron Peikert <aaron.peikert@posteo.de>
2020-10-13 13:33:38 +0200

    initial

---
+++ cheat.sh
@@ -0,0 +1,3 @@
+git checkout 02dynamic-documents && git reset --hard origin/02dynamic-documents
+git checkout 03external-dependencies && git reset --hard origin/03external-dependencies
+git checkout 04internal-dependencies && git reset --hard origin/04internal-dependencies
